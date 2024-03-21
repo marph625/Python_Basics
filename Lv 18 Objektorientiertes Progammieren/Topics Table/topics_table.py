efde864a91da942d4fe7840402a1513b6978a8b0
marph625 <marvin.gnesner@gmail.com>
2024-03-21 14:30:17 +0100

    Classes and dictionaries

--- Lv 18 Objektorientiertes Progammieren/Topics Table/topics_table.py
+++ Lv 18 Objektorientiertes Progammieren/Topics Table/topics_table.py
@@ -2,9 +2,9 @@ from prettytable import PrettyTable
 
 table = PrettyTable()
 
-table.add_column("Number", ["1"])
-table.add_column("Topic", ["Placeholder"])
-table.add_column("Content", ["Another one"])
-table.align = "l"
+table.add_column("Number",["1","1","1","1","1","2","2","2","3","3","3","4","4","4","5","5","5","5"])
+table.add_column("Topic",["UML","UML","UML","UML","UML","Grundlagen Programmieren","Grundlagen Programmieren","Grundlagen Programmieren","Grundlagen Netzwerke","Grundlagen Netzwerke","Grundlagen Netzwerke","SQL","SQL","SQL","Pattern","Pattern","Pattern","Pattern"])
+table.add_column("Content",["Klassendiagramme","Anwendungsfalldiagramme (Use Case)","Zustandsdiagramme","Aktivitätsdiagramme","Sequenzdiagramme","PAP","Pseudocode","Struktorgramm","IPv4", "IPv6", "OSI","ERD", "RDBM","Normalisierung","Observer","Singleton","Factory","MVC"])
+table.align ="l"
 
 print(table)
