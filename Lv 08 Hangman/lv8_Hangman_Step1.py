d2f1437b27e0194b35266eae09248c81cd26fb9d
marph625 <marvin.gnesner@gmail.com>
2024-03-21 08:22:23 +0100

    hangman

--- Lv 08 Hangman/lv8_Hangman_Step1.py
+++ Lv 08 Hangman/lv8_Hangman_Step1.py
@@ -1,3 +1,5 @@
+import random
+
 #   _  _                                
 #  | || |__ _ _ _  __ _ _ __  __ _ _ _  
 #  | __ / _` | ' \/ _` | '  \/ _` | ' \ 
@@ -6,10 +8,31 @@
 
 # Step 1
 
-word_list = ["Dragonball", "JavaScript", "Python", "Programmieren"]
+word_list = ["Dragonball", "JavaScript", "Python", "Programmieren", "One Piece"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
+"""counter = 1
+
+while counter != 6:
+    chosen_word = random.choice(word_list)
+    print(counter, "=>", chosen_word)
+    counter += 1
+"""
+# make chosen_word lowercase to avoid issues when checking
+print("Beepboop! Calculating...choosing word...finished!")
+chosen_word = random.choice(word_list).lower()
+print(chosen_word)
+
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
+guess = input("Guess a letter\n")
+guess = guess.lower()
+print(guess)
+
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+if guess in chosen_word:
+    print(guess, "is in", chosen_word)
+else:
+    print("Try again")
