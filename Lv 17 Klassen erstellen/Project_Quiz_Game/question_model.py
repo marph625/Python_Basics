1d680e4d1180d7ea6aa5eeb0f90c0365d2d8ee81
marph625 <marvin.gnesner@gmail.com>
2024-03-21 16:25:38 +0100

    OOP and Quiz

---
+++ Lv 17 Klassen erstellen/Project_Quiz_Game/question_model.py
@@ -0,0 +1,19 @@
+from data import question_data
+
+# Klasse 
+class Question:
+    # Konstruktor
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+# Objekte
+#q1 = Question("1+1=?", "2")
+#q2 = Question("Ich bin eine Frage", "Ich bin eine Antwort")
+
+#q3 = Question(question_data[0], question_data[1])
+
+# Ausgabe
+#print(q1.text, "->", q1.answer)
+#print(q2.text, "->", q2.answer)
+
+#print(q3.text, "->", q3.answer)
---
+++ Lv 17 Klassen erstellen/Project_Quiz_Game/quiz_controller.py
@@ -0,0 +1,6 @@
+from main import question_bank
+
+class Quizcontroller:
+    def __init__(self, question_number, question_list) -> None:
+        self.question_number = question_number
+        self.question_list = question_bank
