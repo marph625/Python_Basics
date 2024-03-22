12978e63dbf7012a1debd3cd97604be46c039228
marph625 <marvin.gnesner@gmail.com>
2024-03-22 11:59:31 +0100

    Quiz läuft

--- Lv 17 Klassen erstellen/Project_Quiz_Game/main.py
+++ Lv 17 Klassen erstellen/Project_Quiz_Game/main.py
@@ -1,11 +1,19 @@
 from question_model import Question
 from data import question_data
+from quiz_controller import Quizcontroller
 
 question_bank = []
 
+# Für jedes Dictionary in der Liste
 for question in question_data:
+    # Key und Value werden mit dieser Syntax separat in jeweils einer Variable gespeichert
     question_text = question["text"]
     question_answer = question["answer"]
+    
+    # Der Liste wird ein Objekt der Klasse Question hinzugefügt
     question_bank.append(Question(question_text, question_answer))
     
-print(question_bank[0].text, "->", question_bank[0].answer)
+quiz = Quizcontroller(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
--- Lv 17 Klassen erstellen/Project_Quiz_Game/quiz_controller.py
+++ Lv 17 Klassen erstellen/Project_Quiz_Game/quiz_controller.py
@@ -1,6 +1,26 @@
-from main import question_bank
-
 class Quizcontroller:
-    def __init__(self, question_number, question_list) -> None:
-        self.question_number = question_number
-        self.question_list = question_bank
+    def __init__(self, question_list) -> None:
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+        
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Frage: {self.question_number} : {current_question.text} True oder False\n")
+        
+        # Methode check_answer wird hier aufgerufen, um den Zugriff auf die Argumente (für mich) zu erleichtern
+        self.check_answer(user_answer, current_question.answer)
+        
+    def still_has_questions(self) -> bool:
+        return self.question_number < len(self.question_list)
+    
+    # Parameter kann ich nennen wie ich will
+    def check_answer(self, x, y) -> bool:
+        if x.lower() == y.lower():
+            self.score += 1
+            print("Richtig! | Score:", self.score)
+            return True
+        else:
+            print("Falsch! | Score:", self.score)
+            return False
