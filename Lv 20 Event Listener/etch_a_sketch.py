8b3ed0c1d49dd695179b9e7018e255e7416cabb7
marph625 <marvin.gnesner@gmail.com>
2024-03-23 19:02:21 +0100

    added car object with some functionalities

--- Lv 20 Event Listener/etch_a_sketch.py
+++ Lv 20 Event Listener/etch_a_sketch.py
@@ -1,21 +1,36 @@
 from turtle import Turtle, Screen
+import random
 
+# OBJECTS
 little_dude = Turtle()
+car = Turtle()
 screen = Screen()
 
+# SCREEN
+screen.bgcolor("yellow")
+screen.title("Tina the Turtle")
+
+# PLAYER
 little_dude.shape("turtle")
 little_dude.color("green")
 little_dude.shapesize(3)
 little_dude.pensize(10)
 little_dude.pencolor('#3D2B1F')
-screen.bgcolor("yellow")
-screen.title("Tina the Turtle")
 little_dude.pu()
 little_dude.setposition(0, -400)
 little_dude.left(90)
 
+# CAR
+color_list = ["aquamarine", "aquamarine4", "bisque4", "blue", "BlueViolet", "brown1", "CadetBlue1", "chartreuse", "chartreuse4", "chocolate", "coral2",
+            "DarkOliveGreen3", "DarkGoldenrod1", "DarkGreen", "DarkOrange1", "DarkRed", "DarkSeaGreen3", "DarkSlateGray", "DeepPink2", "DarkTurquoise", "DeepSkyBlue"]
+car.shape("square")
+car.color(color_list[3])
+car.shapesize(3)
+car.pu()
+car.setposition(0, 400)
+
 def mv_fd():
-    little_dude.fd(20)
+    little_dude.fd(200)
     
 def mv_bd():
     little_dude.bk(20)
@@ -33,12 +48,18 @@ def pen_down():
     little_dude.pd()
 
 def clear_screen():
+    # player position reset
     little_dude.clear()
     little_dude.pu()
     little_dude.home()
     little_dude.left(90)
     little_dude.setposition(0, -400)
-    little_dude.pd()
+
+    # car position reset
+    car.clear()
+    car.pu()
+    car.home()
+    car.setposition(0, 400)
 
 screen.listen()
 
