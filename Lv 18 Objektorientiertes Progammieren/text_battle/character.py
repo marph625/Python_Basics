0f156756cd550b3599e9b9bbb63f4d2b5c381ca3
marph625 <marvin.gnesner@gmail.com>
2024-03-28 10:58:36 +0100

    text battle with healthbar

--- Lv 18 Objektorientiertes Progammieren/text_battle/character.py
+++ Lv 18 Objektorientiertes Progammieren/text_battle/character.py
@@ -1,4 +1,5 @@
 from weapon import fists
+from health_bar import HealthBar
 
 class Character:
     # Class Level Variables
@@ -15,3 +16,28 @@ class Character:
         target.health -= self.weapon.damage
         # if target.hp is negative, it will be set to 0
         target.health = max(target.health, 0)
+        target.health_bar.update()
+        print(f"{self.name} dealt {self.weapon.damage} damage to " 
+        f"{target.name} with {self.weapon.name}")
+
+class Hero(Character):
+    def __init__(self, name: str, health: int) -> None:
+        super().__init__(name=name, health=health)
+
+        self.default_weapon = self.weapon
+        self.health_bar = HealthBar(self, color="green")
+
+    def equip(self, weapon) -> None:
+        self.weapon = weapon
+        print(f"{self.name} equipped {self.weapon.name}!")
+
+    def drop(self) -> None:
+        self.weapon = self.default_weapon
+        print(f"{self.name} dropped {self.weapon.name}!")
+
+class Enemy(Character):
+    def __init__(self, name: str, health: int, weapon: str) -> None:
+        super().__init__(name=name, health=health)
+
+        self.weapon = weapon
+        self.health_bar = HealthBar(self, color="red")
--- Lv 18 Objektorientiertes Progammieren/text_battle/main.py
+++ Lv 18 Objektorientiertes Progammieren/text_battle/main.py
@@ -1,18 +1,23 @@
-from character import Character
+import os
+from character import Hero, Enemy
+from weapon import short_bow, iron_sword
 
 # objects
 
-hero = Character(name="Gregor", health=100)
-enemy = Character("Hugo", 100)
+hero = Hero(name="Gregor", health=100)
+enemy = Enemy(name="Hugo", health=100, weapon=short_bow)
+hero.equip(iron_sword)
 
 # main loop
 
 while True:
+    os.system("cls")
+
     hero.attack(enemy)
     enemy.attack(hero)
 
-    print(f"{hero.name}:{hero.health}")
-    print(f"{enemy.name}:{enemy.health}")
+    hero.health_bar.draw()
+    enemy.health_bar.draw()
 
     # Makes the infinite loop less scary
     input()
